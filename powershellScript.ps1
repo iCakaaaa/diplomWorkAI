66180cc9755befaa0f8dc9bd15fd8dad4d44f324
iCakaaaa <129048049+iCakaaaa@users.noreply.github.com>
2023-03-29 23:36:12 +0300

    Update powershellScript.ps1

--- powershellScript.ps1
+++ powershellScript.ps1
@@ -3,15 +3,15 @@ $endpoint = "https://dimplomworkinstanceofme.cognitiveservices.azure.com/"
 
 # Code to call Computer Vision service for image analysis
 $img_file = "dog.png"
-if ($args.count -gt 0 -and $args[0] -eq "dog.png") {
+if ($args.count -gt 0 -and $args[0] -in ("dog.png")) {
     $img_file = $args[0]
 }
 
-$img = "https://github.com/iCakaaaa/diplomWorkAI/raw/main/data/vision/dog.png"
-$headers = @{
-    "Ocp-Apim-Subscription-Key" = $key
-    "Content-Type" = "application/json"
-}
+$img = "https://github.com/iCakaaaa/diplomWorkAI/blob/ea56d3848c302d90bb69cbad25c080e8fae13789/data/vision/dog.png"
+
+$headers = @{}
+$headers.Add("Ocp-Apim-Subscription-Key", $key)
+$headers.Add("Content-Type", "application/json")
 
 $body = @{
     "url" = $img
@@ -25,19 +25,19 @@ $result = Invoke-RestMethod -Method Post `
 
 $analysis = $result | ConvertFrom-Json
 
-Write-Host "`nDescription:"
+Write-Host("`nDescription:")
 foreach ($caption in $analysis.description.captions) {
-    Write-Host $caption.text
+    Write-Host ($caption.text)
 }
 
-Write-Host "`nObjects in this image:"
+Write-Host("`nObjects in this image:")
 foreach ($object in $analysis.objects) {
-    Write-Host " - $($object.object)"
+    Write-Host (" -", $object.object)
 }
 
-Write-Host "`nTags relevant to this image:"
+Write-Host("`nTags relevant to this image:")
 foreach ($tag in $analysis.description.tags) {
-    Write-Host " - $tag"
+    Write-Host (" -", $tag)
 }
 
-Write-Host "`n"
+Write-Host("`n")
