abf4037867c60a82686a6b4f0cea47f2832fd40c
iCakaaaa <129048049+iCakaaaa@users.noreply.github.com>
2023-03-29 23:34:14 +0300

    Update powershellScript.ps1

--- powershellScript.ps1
+++ powershellScript.ps1
@@ -1,44 +1,43 @@
-$key="2313f0fb3cc744fb90220a8e3af070d8"
-$endpoint="https://dimplomworkinstanceofme.cognitiveservices.azure.com/"
+$key = "2313f0fb3cc744fb90220a8e3af070d8"
+$endpoint = "https://dimplomworkinstanceofme.cognitiveservices.azure.com/"
 
 # Code to call Computer Vision service for image analysis
 $img_file = "dog.png"
-if ($args.count -gt 0 -And $args[0] -in ("dog.png", "dog.png", "dog.png", "dog.png"))
-{
+if ($args.count -gt 0 -and $args[0] -eq "dog.png") {
     $img_file = $args[0]
 }
 
-$img = "https://github.com/iCakaaaa/diplomWorkAI/blob/ea56d3848c302d90bb69cbad25c080e8fae13789/data/vision/dog.png"
-        
-$headers = @{}
-$headers.Add("Ocp-Apim-Subscription-Key", $key)
-$headers.Add("Content-Type", "application/json")
+$img = "https://github.com/iCakaaaa/diplomWorkAI/raw/main/data/vision/dog.png"
+$headers = @{
+    "Ocp-Apim-Subscription-Key" = $key
+    "Content-Type" = "application/json"
+}
 
-$body = @{url = $img} | ConvertTo-Json
+$body = @{
+    "url" = $img
+} | ConvertTo-Json
 
 Write-Host "Analyzing image..."
 $result = Invoke-RestMethod -Method Post `
           -Uri "$endpoint/vision/v3.2/analyze?visualFeatures=Categories,Description,Objects" `
           -Headers $headers `
-          -Body $body | ConvertTo-Json -Depth 5
+          -Body $body
 
 $analysis = $result | ConvertFrom-Json
-Write-Host("`nDescription:")
-foreach ($caption in $analysis.description.captions)
-{
+
+Write-Host "`nDescription:"
+foreach ($caption in $analysis.description.captions) {
     Write-Host $caption.text
 }
 
-Write-Host("`nObjects in this image:")
-foreach ($object in $analysis.objects)
-{
+Write-Host "`nObjects in this image:"
+foreach ($object in $analysis.objects) {
     Write-Host " - $($object.object)"
 }
 
-Write-Host("`nTags relevant to this image:")
-foreach ($tag in $analysis.description.tags)
-{
+Write-Host "`nTags relevant to this image:"
+foreach ($tag in $analysis.description.tags) {
     Write-Host " - $tag"
 }
 
-Write-Host("`n") 
+Write-Host "`n"
